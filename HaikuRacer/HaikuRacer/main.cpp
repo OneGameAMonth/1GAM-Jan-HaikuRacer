55ec75824e2667f7e9e9a17643c9db5572a9eef9
Kyle Halladay <k.mj.halladay@gmail.com>
2012-12-31 00:02:23 -0500

    initial commit of xcode proj

---
+++ HaikuRacer/HaikuRacer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HaikuRacer
+//
+//  Created by Kyle Halladay on 12-12-31.
+//  Copyright (c) 2012 Team Chilly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
